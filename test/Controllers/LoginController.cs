6d1557c8780e9f48910baaa90999103ba734520c
T00226053 <liam.oconnor-collum@students.ittralee.ie>
2023-10-17 22:08:01 +0100

    Add project files.

---
+++ test/Controllers/LoginController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using test.Models;
+
+namespace test.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult LoginHTML()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult LoginHTML(LoginModel model)
+        {
+            return View();
+        }
+
+        public IActionResult LoginTag()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult LoginTag(LoginModel model)
+        {
+            return View();
+        }
+    }
+}
