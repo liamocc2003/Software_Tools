d8d4c3681449dc1828147f7862b1c7ee64013260
Liam O Connor-Collum <Liam.OConnor-Collum@students.ittralee.ie>
2023-10-26 10:33:46 +0100

    Edit Order MVC created

---
+++ test/Controllers/EditOrderController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace test.Controllers
+{
+    public class EditOrderController : Controller
+    {
+        public IActionResult EditOrder()
+        {
+            return View();
+        }
+    }
+}
---
+++ test/Models/EditOrderModel.cs
@@ -0,0 +1,8 @@
+﻿namespace test.Models
+{
+    public class EditOrderModel
+    {
+        public LinkedList<String> orderItems = new LinkedList<String>();
+        public LinkedList<int> orderPrices = new LinkedList<int>();
+    }
+}
