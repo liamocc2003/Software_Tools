9db3073a0f65123b49ebed46f9101553b170d93f
T00226053 <liam.oconnor-collum@students.ittralee.ie>
2023-11-05 16:37:07 +0000

    Edit Orders started with Create Orders template

---
+++ test/Controllers/EditOrdersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace test.Controllers
+{
+    public class EditOrdersController : Controller
+    {
+        public IActionResult EditOrders()
+        {
+            return View();
+        }
+    }
+}
---
+++ test/Models/EditOrdersModel.cs
@@ -0,0 +1,9 @@
+﻿namespace test.Models
+{
+    public class EditOrdersModel
+    {
+        
+        public List<String> OrderItems { get; set; }
+        public List<float> OrderPrices { get; set; }
+    }
+}
