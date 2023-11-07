1efcd40e4b36d734082d0e6a64f6d5618d527714
Alan <alan35691@gmail.com>
2023-11-07 23:07:47 +0000

    Create Order Function + Model and Controller

--- test/Controllers/HomeController.cs
+++ test/Controllers/HomeController.cs
@@ -1,32 +1,78 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
+using System.Collections.Generic;
+using System.Data.SqlClient;
 using test.Models;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
 
 namespace test.Controllers
-{
-    public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        public class HomeController : Controller
         {
-            _logger = logger;
-        }
+            private readonly ILogger<HomeController> _logger;
+            public HomeController(ILogger<HomeController> logger)
+            {
+                _logger = logger;
+            }
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+            public IActionResult Index()
+            {
+                return View();
+            }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+            public IActionResult Privacy()
+            {
+                return View();
+            }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+            [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+            public IActionResult Error()
+            {
+                return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            }
+
+            public IActionResult RetrieveItems()
+{
+    var model = new List<CreateOrder>();
+
+    string connectionString = "YourConnectionStringHere";
+    using (SqlConnection connection = new SqlConnection(connectionString))
+    {
+        connection.Open();
+        string sql = "SELECT Name, Price FROM MenuItems";
+        using (SqlCommand command = new SqlCommand(sql, connection))
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            try
+            {
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var orderDetails = new CreateOrder();
+                        orderDetails.itemName = reader.GetString(0);
+                        orderDetails.itemPrice = reader.GetDecimal(1).ToString();
+                        model.Add(orderDetails);
+                        Console.WriteLine(orderDetails.itemName);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
         }
     }
+
+    // Pass the List of CreateOrder to the view with the menu items.
+    return View("Index", model);
 }
+
+        }  
+    }
+
+    public class OrderDetails
+    {
+        public string itemName;
+        public string itemPrice;
+    }
+
---
+++ test/Models/CreateOrder.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Data.SqlClient;
+using test.Controllers;
+
+namespace test.Models
+{
+    public class CreateOrder
+    {
+        public string itemName { get; set; }
+        public string itemPrice { get; set; }
+        public List<OrderDetails> ListOrderDetails { get; set; }
+
+        public CreateOrder()
+        {
+            ListOrderDetails = new List<OrderDetails>();
+        }
+    }
+} 
