b62905467fb52d72b7198ee807caa576c46fc07c
Billy-Elsbury <billy.elsbury@students.ittralee.ie>
2023-11-07 06:22:34 +0000

    Database

--- test/Controllers/PayBillController.cs
+++ test/Controllers/PayBillController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using test.Models; 
+using System.Collections.Generic;
 
 namespace test.Controllers
 {
@@ -6,7 +9,50 @@ namespace test.Controllers
     {
         public IActionResult PayBill()
         {
-            return View();
+            var model = new PayBillModel();
+
+            try
+            {
+                string connectionString = "Data Source=restaurantdatabaseserver.database.windows.net;Initial Catalog=restaurantdb;Persist Security Info=True;User ID=sqladmin;Password=***********";
+
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    string sql = "SELECT OrderID, OrderPrice, OrderStatus FROM ORDERS";
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                OrderDetails orderDetails = new OrderDetails
+                                {
+                                    OrderId = reader.GetInt32(0).ToString(),
+                                    OrderPrice = reader.GetDecimal(1).ToString(), // Assuming orderPrice is a decimal
+                                    OrderStatus = reader.GetString(2) // Assuming orderStatus is a string
+                                };
+
+                                model.ListOrderDetails.Add(orderDetails);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Ideally, log this exception with a logging framework.
+                Console.WriteLine("Exception: " + ex.ToString());
+            }
+
+            // Pass the list to the view as a model.
+            return View(model);
         }
+    }
+
+    public class OrderDetails
+    {
+        public string? OrderId { get; set; }
+        public string? OrderPrice { get; set; }
+        public string? OrderStatus { get; set; }
     }
 }
--- test/Models/PayBillModel.cs
+++ test/Models/PayBillModel.cs
@@ -1,7 +1,17 @@
-﻿namespace test.Models
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Data.SqlClient;
+using test.Controllers;
+
+namespace test.Models
 {
-    public class PayBillModel
-    {
-        
+        public class PayBillModel
+        {
+            public List<OrderDetails> ListOrderDetails { get; set; }
+
+            public PayBillModel()
+            {
+                ListOrderDetails = new List<OrderDetails>();
+            }
+        }
     }
-}
