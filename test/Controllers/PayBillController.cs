448680dc955aaecb834541ea4405a658eadba26f
lukef3 <lukefoley@outlook.com>
2023-11-07 01:04:17 +0000

    Pay Bill UI

---
+++ test/Controllers/PayBillController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace test.Controllers
+{
+    public class PayBillController : Controller
+    {
+        public IActionResult PayBill()
+        {
+            return View();
+        }
+    }
+}
