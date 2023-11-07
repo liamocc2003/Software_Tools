ce01f4ea66abd776ce70582941dd8902ff88c615
Billy-Elsbury <billy.elsbury@students.ittralee.ie>
2023-11-07 03:31:12 +0000

    Billy

---
+++ test/Models/DatabaseModel.cs
@@ -0,0 +1,28 @@
+﻿namespace test.Models
+{
+    public class OrderItem
+    {
+        public int OrderID { get; set; }
+        public string ItemName { get; set; }
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+
+        // Other properties related to OrderItems
+    }
+
+    public class Order
+    {
+        public int OrderID { get; set; }
+        public DateTime OrderDate { get; set; }
+        public decimal OrderPrice { get; set; }
+
+        public List<OrderItem> OrderItems { get; set; }
+
+        // Method to calculate the total order price based on order items
+        public void CalculateTotalPrice()
+        {
+            OrderPrice = OrderItems.Sum(item => item.UnitPrice * item.Quantity);
+        }
+    }
+
+}
---
+++ test/Models/PayBillModel.cs
@@ -0,0 +1,7 @@
+﻿namespace test.Models
+{
+    public class PayBillModel
+    {
+        
+    }
+}
