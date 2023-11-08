09155f28cbe361b8c7f72db3ec5f525a5096689b
lukef3 <lukefoley@outlook.com>
2023-11-08 14:22:07 +0000

    Database added to Pay Bill + Edit Orders Page

--- test/Controllers/PayBillController.cs
+++ test/Controllers/PayBillController.cs
@@ -1,4 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using test.Models;
+using System.Collections.Generic;
+using System.Data;
 
 namespace test.Controllers
 {
@@ -6,7 +10,45 @@ namespace test.Controllers
     {
         public IActionResult PayBill()
         {
-            return View();
+            var model = new PayBillModel();
+
+            {
+                string connectionString = "Server=tcp:restaurantdatabaseserver.database.windows.net,1433;Initial Catalog=restaurantdb;Persist Security Info=False;User ID=adminBilly;Password=Password1;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    string sql = "SELECT orderItem.OrderID, menuItem.ItemName, menuItem.ItemType, menuItem.ItemPrice FROM OrderItems orderItem INNER JOIN MenuItems menuItem ON orderItem.ItemID = menuItem.ItemID WHERE orderItem.OrderID = 2;";
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                OrderDetails orderDetails = new OrderDetails();
+
+                                orderDetails.orderId = "" + reader.GetDecimal(0);
+                                orderDetails.itemName = "" + reader.GetString(1);
+                                orderDetails.itemType = "" + reader.GetString(2);
+                                orderDetails.itemPrice = "" + reader.GetDecimal(3);
+
+                                model.ListOrderDetails.Add(orderDetails);
+                            }
+                        }
+                    }
+                }
+            }
+
+            // Pass the list to the view as a model.
+            return View(model);
         }
+    }
+
+    public class OrderDetails
+    {
+        public string orderId;
+        public string itemName;
+        public string itemType;
+        public string itemPrice;
     }
 }
