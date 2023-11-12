f96677373d4c5a0a99fe4babcbd5bfa02234e0f4
T00226053 <liam.oconnor-collum@students.ittralee.ie>
2023-11-12 23:49:29 +0000

    All Menu Items are pulled from database and displayed in EditOrders. Attempted menu Items displaying by menuType but not functional. Changed css for OrderItems to look better

--- test/Models/EditOrdersModel.cs
+++ test/Models/EditOrdersModel.cs
@@ -3,10 +3,12 @@
     public class EditOrdersModel
     {
         public List<EditOrderDetails> listEditOrderDetails { get; set; }
+        public List<EditOrderDetails> listEditOrderMenuItems { get; set; }
 
         public EditOrdersModel() 
         {
             listEditOrderDetails = new List<EditOrderDetails>();
+            listEditOrderMenuItems = new List<EditOrderDetails>();
         }
     }
 }
