22cb2f91b1b35b39d1d4ff32a9c2a8f5ce67816f
Billy <billy.elsbury@students.ittralee.ie>
2023-11-21 17:05:58 +0000

    Revert "Updated Connection Strings"
    This reverts commit 2cc125d33106d183898ebbe97e70c2de657fd1b3.

--- test/Controllers/FastBar.cs
+++ test/Controllers/FastBar.cs
@@ -36,12 +36,7 @@ namespace test.Controllers
         {
             var model = new CreateOrder();
 
-<<<<<<< Updated upstream
             string connectionString = "Server=tcp:restaurantdatabaseserver.database.windows.net,1433;Initial Catalog=restaurantdb;Persist Security Info=False;User ID=adminBilly;Password=Password1;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
-=======
-            string connectionString = "Server=tcp:restaurantdatabaseserver2.database.windows.net,1433;Initial Catalog=restaurantdb;Persist Security Info=False;User ID=adminBilly;Password=Password1;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
-
->>>>>>> Stashed changes
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
                 connection.Open();
@@ -74,65 +69,6 @@ namespace test.Controllers
             return View(model);
         }
 
-<<<<<<< Updated upstream
-=======
-        [HttpPost]
-        public IActionResult CreateOrder(List<OrderDetails> selectedItems, string customerName)
-        {
-            int orderTableNumber = 1; //hard coded, where do we get table number?
-            string connectionString = "Server=tcp:restaurantdatabaseserver2.database.windows.net,1433;Initial Catalog=restaurantdb;Persist Security Info=False;User ID=adminBilly;Password=Password1;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
-           
-            int orderID = GenerateOrderID();
-
-            string insertOrderQuery = "INSERT INTO Orders (OrderID, OrderTableNO, OrderDate, OrderPrice, OrderStatus) VALUES (@OrderID, @OrderTableNO, GETDATE(), @OrderPrice, @OrderStatus)";
-
-            using (SqlConnection connection = new SqlConnection(connectionString))
-            {
-                connection.Open();
-                using (SqlCommand command = new SqlCommand(insertOrderQuery, connection))
-                {
-                    command.Parameters.AddWithValue("@OrderID", orderID);
-                    command.Parameters.AddWithValue("@OrderTableNO", orderTableNumber);
-                    command.Parameters.AddWithValue("@OrderPrice", selectedItems.Sum(item => decimal.Parse(item.itemPrice))); // Assuming itemPrice is a string
-                    command.Parameters.AddWithValue("@OrderStatus", "A");
-
-                    command.ExecuteNonQuery();
-                }
-                connection.Close();
-            }
-
-            return RedirectToAction("OrderSuccess");
-        }
-
-        private int GenerateOrderID()
-        {
-            int nextOrderID = 0;
-
-            string connectionString = "Server=tcp:restaurantdatabaseserver2.database.windows.net,1433;Initial Catalog=restaurantdb;Persist Security Info=False;User ID=adminBilly;Password=Password1;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"; // Replace with your actual connection string
-
-            using (SqlConnection connection = new SqlConnection(connectionString))
-            {
-                connection.Open();
-
-                string sql = "SELECT MAX(OrderID) FROM Orders";
-
-                using (SqlCommand command = new SqlCommand(sql, connection))
-                {
-                    object result = command.ExecuteScalar();
-
-                    if (result != DBNull.Value)
-                    {
-                        nextOrderID = Convert.ToInt32(result) + 1;
-                    }
-                }
-                connection.Close();
-            }
-
-            return nextOrderID;
-        }
-
-
->>>>>>> Stashed changes
     }
 }
 
